5add805379889660de1e73acdb07db8f48b11d84
Raman Budny <budnyjj@gmail.com>
2016-12-01 22:59:06 +0300

    OTOS:lab3: init

---
+++ disciplines/OTOS/lab_2-3/lab.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import functools
+import math
+import random
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+
+def model(avg, disp, alpha, period, num_iter):
+    Fi = math.exp(-alpha*period)
+    sigma = math.sqrt(disp*(1-math.exp(-2*alpha*period)))
+    cur_val = avg + random.gauss(0, sigma)
+    for _ in range(num_iter):
+        new_val = avg + Fi*(cur_val - avg) + random.gauss(0, sigma)
+        cur_val = new_val
+        yield new_val
+
+def meter(input_value, transform, disp):
+    return transform*input_value + random.gauss(0, math.sqrt(disp))
+
+def filter_kalman(vals, model_avg, model_disp, model_alpha, model_period, meter_transform, meter_disp):
+    result = []
+    Fi = math.exp(-model_alpha*model_period)
+    est_filtered = model_avg
+    disp_filtered = model_disp
+    for val in vals:
+        est_extrapolated = Fi * est_filtered
+        disp_extrapolated = model_disp*(1-math.exp(-2*model_alpha*model_period)) + Fi*disp_filtered*Fi
+        gain = (disp_extrapolated*meter_transform) / (meter_transform*disp_extrapolated*meter_transform + meter_disp)
+        est_filtered = est_extrapolated + gain*(val - meter_transform*est_extrapolated)
+        disp_filtered = disp_extrapolated - gain*meter_transform*disp_filtered
+        result.append(est_filtered)
+    return result
+
+NUM_ITER = 10
+# model parameters
+MODEL_AVG = 0
+MODEL_DISP = 8
+MODEL_ALPHA = 0.008
+MODEL_PERIOD = 40
+# meter parameters
+METER_TRANSFORM = 1
+METER_DISP = 2
+
+
+vals_t = []
+vals_x = []
+vals_m = []
+for i,val_x in enumerate(model(MODEL_AVG, MODEL_DISP,
+                               MODEL_ALPHA, MODEL_PERIOD, NUM_ITER)):
+    vals_t.append(i*MODEL_PERIOD)
+    vals_x.append(val_x)
+    vals_m.append(meter(val_x, METER_TRANSFORM, METER_DISP))
+
+vals_fm = filter_kalman(vals_m,
+                        MODEL_AVG, MODEL_DISP, MODEL_ALPHA, MODEL_PERIOD,
+                        METER_TRANSFORM, METER_DISP)
+
+plt.plot(vals_t, vals_x,
+         color='r', linestyle=' ',
+         marker='.', markersize=5)
+plt.plot(vals_t, vals_m,
+         color='g', linestyle='-')
+plt.plot(vals_t, vals_fm,
+         color='b', linestyle='-')
+
+
+plt.grid(True)
+plt.xlabel('$ t $')
+plt.ylabel('$ x $')
+plt.savefig('plot.png', dpi=200)
